752d75381fb66526fa990a1835d53632fc40690c
PM <dusthillresident@gmail.com>
2021-02-01 22:01:16 +0000

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # -lXext for the Xdbe double buffer X extension
+echo "// DO NOT EDIT THIS, instead edit tokenslist_src.c" > tokenslist.c
+if [[ -e gfxbin ]]
+then 
+ cat tokenslist_src.c | gfxbin PENIStool.johnson >> tokenslist.c
+fi
 gcc -Ofast $1 -Denable_graphics_extension sl.c  -lm -lXext -lX11 -lpthread -o gfxbin
